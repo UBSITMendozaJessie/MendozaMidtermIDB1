dc1b19df58598bfe798ab74c701ae8db512c636a
Jessie Mendoza <20214135@s.ubaguio.edu>
2023-03-29 13:48:22 +0800

    cwommit

---
+++ MendozaME/src/app/category-list.ts
@@ -0,0 +1,16 @@
+import { Category } from "./category";
+
+export const CATEGORY: Category[]=[
+    {   id: 1,
+        name: 'Tomas',
+        description: 'Lorem'
+    },
+    {   id: 2,
+        name: 'Edward',
+        description: 'Lorem'
+    },
+    {   id: 3,
+        name: 'Mark',
+        description: 'Lorem'
+    }
+    ]
---
+++ MendozaME/src/app/product.ts
@@ -0,0 +1,10 @@
+export interface Product{
+    id: number;
+    name: string;
+    category: string;
+    dateAdded: Date;
+    sizes: string;
+    colors: string;
+    brand: string;
+    sPic: string;
+}
